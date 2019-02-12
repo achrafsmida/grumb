b652642567ad4504f725f7892e4a6e9f02e82dd4
achraf <achrafsmida@hotmail.fr>
2019-02-12 15:43:44 +0100

    update

--- my_project_name/src/AppBundle/Controller/DefaultController.php
+++ my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
 
 
         return $this->render('default/index.html.twig', [
-            
+
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
