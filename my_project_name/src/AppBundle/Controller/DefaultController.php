60617e00e5bf09e6039e932597bc39cafc98e76b
achraf <achrafsmida@hotmail.fr>
2019-02-12 15:44:53 +0100

    update

--- my_project_name/src/AppBundle/Controller/DefaultController.php
+++ my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class defaultController extends Controller
+class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
@@ -14,10 +13,7 @@ class defaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-
-
         return $this->render('default/index.html.twig', [
-
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
